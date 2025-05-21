9cc16b0d6a760650c7545631732bb443344ff6b5
mahammad-aimlabs <mahammad.sathyala@aimlabs.in>
2025-05-21 16:57:52 +0530

    removed some games

--- apps/scatter-old/svelte.config.js
+++ apps/scatter-old/svelte.config.js
@@ -1,4 +1,26 @@
 // @ts-ignore
-import config from 'config-svelte';
+// import config from 'config-svelte';
 
-export default config();
+// export default config();
+
+
+//Newly Added Config for our deployment
+
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+//Must update game name when we need builds inside specific gameName folder
+const gameName="scatter-old";
+export default {
+    preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: `../../static/apps/${gameName}`,
+			assets: `../../static/apps/${gameName}`,
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
